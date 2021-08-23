96059824731ded86217fcb58ef69f4ca6e372caa
jqh-alwustha <88532026+jqh-alwustha@users.noreply.github.com>
2021-08-23 16:23:03 +0700

    Diklat X v.0.1
    Belum lengkap infonya

---
+++ reveal.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.section-container', { delay: 500 });
+ScrollReveal().reveal('.jargon', { delay: 500 });
+ScrollReveal().reveal('.lomba', { delay: 500 });
+ScrollReveal().reveal('.bio', { delay: 500 });
+ScrollReveal().reveal('#profile-yt', { delay: 500 });
