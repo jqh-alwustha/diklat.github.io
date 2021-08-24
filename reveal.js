af142e41434e3c024bb32f0ee2788658347cee31
jqh-alwustha <88532026+jqh-alwustha@users.noreply.github.com>
2021-08-24 08:08:06 +0700

    Update reveal.js

--- reveal.js
+++ reveal.js
@@ -1,5 +1,6 @@
-ScrollReveal().reveal('.section-container', { delay: 500 });
 ScrollReveal().reveal('.jargon', { delay: 500 });
-ScrollReveal().reveal('.lomba', { delay: 500 });
-ScrollReveal().reveal('.bio', { delay: 500 });
+ScrollReveal().reveal('.wrapper', { delay: 500 });
+ScrollReveal().reveal('.slideshow-container', { delay: 500 });
+ScrollReveal().reveal('.container-card', { delay: 500 });
+ScrollReveal().reveal('.subsection', { delay: 500 });
 ScrollReveal().reveal('#profile-yt', { delay: 500 });
